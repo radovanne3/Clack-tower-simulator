8cfc0808dcf9d421f4a46d9888e20b45bdba472e
Snorlax <radovanne3@gmail.com>
2021-11-11 16:06:49 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clack-tower-simulator
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/snorlax/clack_tower_simulator_test.clj
@@ -0,0 +1,7 @@
+(ns snorlax.clack-tower-simulator-test
+  (:require [clojure.test :refer :all]
+            [snorlax.clack-tower-simulator :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
